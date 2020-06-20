83d261f3e157afd6083994b8ba873fa239e72478
Krishnachaitanya <kpullak@ncsu.edu>
2020-06-20 02:05:43 -0400

    Modified

--- combine_data.py
+++ combine_data.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+
+Created on Thursday June 18 17:38:40 2020
+@author: Krishna
+
+Implementation of the health news tweet clustering using the
+Jaccard Distance metric and K-means clustering algorithm to
+cluster redundant/repeated tweets into the same cluster.
+
+"""
 
 import glob
 
